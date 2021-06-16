f289476997e5228c06ff8b98deb554269e8f33f9
02Azure <agusdomas1st@gmail.com>
2021-06-16 12:57:06 +0700

    finalizing basic css users & home page

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 
 export default function Home(props) {
+  let lastUpdate = new Date(props.updated_at)
+  lastUpdate = lastUpdate.toLocaleString("en-US")
   return(
     <div class="repo-card">
       <a class="repo-title" href={ props.html_url } target="_blank"><h2>{ props.name }</h2></a>
       <div class="repo-detail">
-        <p>Language: { props.language }</p>
-        <p>Last Update: { props.updated_at }</p>
-        <p>{ props.description }</p>
+        <p><span class="bold-text">Language:</span> { props.language }</p>
+        <p><span class="bold-text">Last Update:</span> { lastUpdate }</p>
+        <p class="repo-desc">{ props.description }</p>
       </div>
 
     </div>
--- src/components/UserBox.jsx
+++ src/components/UserBox.jsx
@@ -3,14 +3,16 @@ import React from "react"
 export default function UserBox(props) {
   return(
     <div id="userbox">
-      <img id="user-avatar" src={ props.avatar_url } alt="user's avatar" />
+      <div id="avatar-container">
+        <img id="user-avatar" src={ props.avatar_url } alt="user's avatar" />
+      </div>
       <a href={ props.html_url } target="_blank"><h2>{ props.login }</h2></a>
       { props.name && 
         <h3>({ props.name })</h3>
       }
       <p>{ props.bio }</p>
-      <p>{ props.followers } Followers</p>
-      <p>{ props.following } Following</p>
+      <p class="bold-text">{ props.followers } Followers</p>
+      <p class="bold-text">{ props.following } Following</p>
     </div>
   )
 }
